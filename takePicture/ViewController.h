2996e8e3d6bdc96a6216e09348d0592f2bb3df03
chiunti <usuario@C3.local>
2015-01-22 20:35:13 -0600

    Initial Commit

---
+++ takePicture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  takePicture
+//
+//  Created by Chiunti on 22/01/15.
+//  Copyright (c) 2015 chiunti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
